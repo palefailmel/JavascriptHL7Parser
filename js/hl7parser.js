e804b01dd13d157eda91417c3309b2f529066e4f
MikeAStevenson <siscodotnet@gmail.com>
2020-07-02 22:17:49 -0500

    Redefining HL7Parser. Stripping out HTML dependent code. Beginning replacement of JSON message object.

--- js/hl7parser.js
+++ js/hl7parser.js
@@ -10,12 +10,35 @@ var HL7Parser = {
         PAGE: 'PAGE',
         JSON: 'JSON'
     },
+    message     : {},
     fieldSep    : '|',
     compSep     : '^',
     subCompSep  : '&',
     repeatSep   : '~',
     escapeChar  : '\\',
-    outputType  : OUTPUT_TYPES.PAGE, // default to basic page output
+
+    // entry point into processing
+    tryParse    : function(inboundMessage, outputType, resultAreaId) {
+        let msg = inboundMessage.trim();
+
+        console.log('this is a test');
+
+        // Check if string is empty or doesn't begin with MSH
+        if (msg === '' || msg.substring(0,3) != 'MSH') {
+            // do something
+        } else {
+            this.originalMsg = msg;
+            this.setSeparators();
+            this.separateSegs();
+            this.parseSegs();
+
+            for (segment in this.segments) {
+                console.log(this.segments[segment]);
+            }
+
+            console.log(JSON.stringify(this.message));
+        }
+    },
     
     // replace separates with actual separats from message
     setSeparators : function() {
@@ -34,7 +57,11 @@ var HL7Parser = {
         }
     },
     // parse out segments
-    parseSegs : function(segFields) {
+    // places results in message object
+    parseSegs : function() {
+        let myMsg = {};
+        myMsg.segments = {};
+
         for (let i = 0; i < this.segments.length; i++) {
             let currSeg         = this.segments[i];
             let currSegFields   = currSeg.split(this.fieldSep);
@@ -43,66 +70,65 @@ var HL7Parser = {
             // this will always happen if the segment ends with the field sep
             if (currSegFields[(currSegFields.length - 1)] == '') currSegFields.splice(-1,1);
 
+
             // If we're at the MSH, correct segment numbering and set the info header
             if (currSegFields[0] == 'MSH') {
                 currSegFields[0] = this.fieldSep; //Replace the array's first value "MSH" with the Field Separator
                 currSegFields.unshift("MSH"); // Add back "MSH" as the array's first value
                 // set the info header
-                this.addMSHHeader(currSegFields);
+                //this.addMSHHeader(currSegFields);
             }
 
-            // add segment header for each segment
-            this.addSegHeader(currSegFields);
-            // add segment field information
-            this.addSegFields(currSegFields);
-        }
-    },
-    // output segment header
-    // flex type of output based on outputType variable
-    addSegHeader : function(segFields) {
-        switch(this.outputType) {
-            case OUTPUT_TYPES.PAGE:
-                let headerObj       = document.createElement('h4');
-                let headerString    = '';
-                // display what type of segment it is
-                headerString        += segFields[0] + " segment - ";
-                // display how many fields are in the segment
-                // (without counting the first item, which is just the segment name)
-                headerString        += (segFields.length-1) + " fields";
-                headerObj.innerHTML = headerString
-                // add segment header to bottom of page
-                this.resultsArea.appendChild(headerObj);
-
-                // add segment to seg header
-                let preObj = document.createElement('pre');
-                preObj.innerHTML = segFields.join(this.fieldSep);
-                this.resultsArea.appendChild(preObj);
-                break;
-        }
-    },
-    addMSHHeader : function(mshSegFields) {
-        switch(this.outputType) {
-            case OUTPUT_TYPES.PAGE:
-            let headerObj       = document.createElement('h3');
-            let headerString    = '';
-            // add message type to header
-            headerString        += "Message Type: " + mshSegFields[8];
-            // add HL7 version to header
-            headerString        += " - HL7 Version: " + mshSegFields[11];
-            // add datetime in MSH segment to header
-            headerString        += " - Date/Time of Msg: " + mshSegFields[6];
-            headerObj.innerHTML = headerString
-            // add to page
-            this.resultsArea.appendChild(headerObj);
+            let segObj = {};
+            segObj.segmentType = currSegFields[0];
+            segObj.fieldCount = (currSegFields.length-1);
+            //segObj.segmentFullText = (currSeg);
+            segObj.field = {};
             
-            // go ahead and add message as well
-            let segs = this.originalMsg.split('\n');
-            for (let i = 0, size = segs.length; i < size; i++) {
-                let preObj = document.createElement('pre');
-                preObj.innerHTML = segs[i];
-                this.resultsArea.appendChild(preObj);
+            // iterate through the fields in the message
+            // skip the segment type
+            currSegFields.shift();
+            for (let j = 0; j < currSegFields.length; j++) {
+                let field = currSegFields[j];
+
+                // check for any repeating fields 
+                let componentSets = field.split(this.repeatSep);
+                segObj.field[j] = {};
+
+                for (let compSet of componentSets) {
+                    console.log(compSet);
+                }
+
+                let components = field.split(this.compSep);
+
+                segObj.field[j] = {};
+                
+                segObj.field[j].components = {};
+
+                // loop through the components in each field to parse them out
+                for (let comp in components) {
+                    let currComp = components[comp];
+
+                    segObj.field[j].components[comp] = currComp;
+
+                    // see if there are any subcomponents
+                }
+
+                //segObj.field[currField] = currSegFields[currField];
             }
-            break;
+
+            myMsg.segments[i] = (segObj);
         }
+
+        this.message = myMsg;
     }
-}
+};
+
+let testMsg = 'TVNIfF5+XCZ8RVBJQ3xFUElDQURUfFNNU3xTTVNBRFR8MTk5OTEyMjcxNDA4fENIQVJSSVN8QURUXkEwNHwxODE3NDU3fER8ICAyLjV8ClBJRHx8MDQ5MzU3NV5eXjJeSUQgMXw0NTQ3MjF8fERPRV5KT0hOXl5eXnxET0VeSk9ITl5eXl58MTk0ODAyMDN8TXx8QnwyNSAgNCBNWVNUUkVFVCBBVkVeXk1ZVE9XTl5PSF40NDEyM15VU0F8fCgyMTYpMTIzLTQ1Njd8fHxNfE5PTnw0MDAwMDM0MDN+MTEyOTA4NnwKTksxfHxST0VeTUFSSUVeXl5efFNQT3x8KDIxNikxMjMtNDU2N3x8RUN8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHwKUFYxfHxPfDE2OCB+MjE5fkN+UE1BXl5eXl5eXl5efHx8fDI3N15BTExFTiBNWUxBU1ROQU1FXkJPTk5JRV5eXl58fHx8fHx8ICB8fHwgfHwyNjg4Njg0fHx8fHxvbmVedGVzdCZ0aHJlZSZmb3VyfHx8fHx8fHx8fHx8fHx8fHx8fHwxOTk5MTIyNzE0MDh8fHx8fHwwMDIzNzY4NTM=';
+
+let myTest = 'MSH|^~\&|EPIC|EPICADT|SMS|SMSADT|199912271408|CHARRIS|ADT^A04|1817457|D|  2.5|\n'+
+'PID||0493575^^^2^ID 1|454721||DOE^JOHN^^^^|DOE^JOHN^^^^|19480203|M||B|25  4 MYSTREET AVE^^MYTOWN^OH^44123^USA||(216)123-4567|||M|NON|400003403~1129086|\n'+
+'NK1||ROE^MARIE^^^^|SPO||(216)123-4567||EC|||||||||||||||||||||||||||\n'+
+'PV1||O|168 ~219~C~PMA^^^^^^^^^||||277^ALLEN MYLASTNAME^BONNIE^^^^|||||||  ||| ||2688684|||||one^test&three&four||||||||||||||||||||199912271408||||||002376853';
+
+HL7Parser.tryParse(myTest,HL7Parser.OUTPUT_TYPES.PAGE, '');
